94c4fff9891eb59e20078f24c48888d901e1d497
hailee911 <167476418+hailee911@users.noreply.github.com>
2025-02-26 14:35:57 +0900

    메인 페이지 추가 (html,css,images)

--- WeverseEgg0.2/src/main/java/com/java/controller/FController.java
+++ WeverseEgg0.2/src/main/java/com/java/controller/FController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FController {
 	
-	@GetMapping("/")
+	@GetMapping("/index")
 	public String index() {
 		return "index";
+	}
+	
+	@GetMapping("/") // 메인 게임 시작 페이지
+	public String eggMain() {
+		return "egg_main";
 	}
 	
 }
