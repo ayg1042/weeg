b8944f41a1154cf9e5b8fb11cf4abe988cca873b
sarah3832 <sarah38320@gmail.com>
2025-03-07 16:12:39 +0900

    sy

--- WeverseEgg0.2/src/main/java/com/java/entity/character/ArtistEntity.java
+++ WeverseEgg0.2/src/main/java/com/java/entity/character/ArtistEntity.java
@@ -3,10 +3,7 @@ package com.java.entity.character;
 import java.sql.Timestamp;
 
 import com.java.entity.group.ArtistNameEntity;
-<<<<<<< HEAD
 import com.java.entity.group.GroupEntity;
-=======
->>>>>>> refs/remotes/origin/master
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
